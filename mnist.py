fix softmax

mnist.py
@@ -3,6 +3,7 @@ from layers.fc_layer import FCLayer
 from layers.softmax_layer import SoftmaxLayer
 from activations.tanh import TanH
 from activations.relu import Relu
+from activations.leaky_relu import LeakyRelu
 from activations.softmax import Softmax
 from activations.sigmoid import Sigmoid
 from losses.cross_entropy import CrossEntropy
@@ -22,6 +23,7 @@ from keras.utils import np_utils
 X = x_train.reshape(x_train.shape[0], 28*28).T
 X = X.astype('float32')
 X /= 255
+
 # encode output which is a number in range [0,9] into a vector of size 10
 # e.g. number 3 will become [0, 0, 0, 1, 0, 0, 0, 0, 0, 0]
 Y = np_utils.to_categorical(y_train)
@@ -31,27 +33,36 @@ Y = np_utils.to_categorical(y_train)
 net = NeuralNetwork()
 net.add(FCLayer(28*28, 100, activation=TanH()))
 net.add(FCLayer(100, 50, activation=TanH()))
-net.add(FCLayer(50, 10, activation=Softmax()))
+net.add(FCLayer(50, 25, activation=TanH()))
+net.add(SoftmaxLayer(25, 10, activation=Softmax()))
 
 # train
 net.use(loss=MultiClassCrossEntropy())
-net.train(X[:, :2000], Y[0:2000].T, epochs=1000, learning_rate=1.2)
-
-#print(net.get_acc(X, Y))
+train_results = net.train(X[:,:20000], Y[0:20000].T, epochs=500, learning_rate=1.2)
+print(train_results.shape)
+print(Y[0:20000].T.shape)
+train_results = np.argmax(train_results, axis = 1)
+train_labels = np.argmax(Y[:,:20000].T, axis = 1)
+# print(train_results.shape)
+# print(train_labels.shape)
+# print(train_results)
+# print(train_labels)
+# print("Accuracy on train set:", np.mean(train_results == train_labels), "out of 1")
 
 # same for test data : 10000 samples
-x_test = x_test.reshape(x_test.shape[0], 28*28)
+x_test = x_test.reshape(x_test.shape[0], 28*28).T
 x_test = x_test.astype('float32')
 x_test /= 255
 y_test = np_utils.to_categorical(y_test)
 
-out = net.predict(x_test[0:10].T)
+out = net.predict(x_test)
 print("\n")
 print("predicted values : ")
 print(out, end="\n")
 print(np.argmax(out, axis=0))
-print("true values : ")
-print(y_test[0:10])
 print("true labels: ")
-print(np.argmax(y_test[0:10],axis=1))
+print(np.argmax(y_test,axis=1))
+x = np.argmax(out, axis=0)
+y = np.argmax(y_test,axis=1)
+print("Accuracy on testing set:", np.mean(x == y), "out of 1")
 

xor.py
@@ -7,26 +7,39 @@ from losses.cross_entropy import CrossEntropy
 import numpy as np
 
 # Create our XOR dataset
-X = np.random.randint(2, size = (100, 2))
+X = np.random.randint(2, size = (1000, 2))
 
 mask1 = X[:, 0] > 0.5
 mask2 = X[:, 1] > 0.5
 
 Y = np.logical_xor(mask1, mask2)
-Y = Y.reshape(100, 1)
+Y = Y.reshape(1000, 1)
 
 X = X.T
 Y = Y.T
 
 # Create our NN structure
 net = NeuralNetwork()
-net.add(FCLayer(2, 3, activation=TanH()))
-net.add(FCLayer(3, 2, activation=Sigmoid()))
+net.add(FCLayer(2, 5, activation=TanH()))
+net.add(FCLayer(5, 2, activation=Sigmoid()))
 
 # train
 net.use(loss=CrossEntropy())
 net.train(X, Y, epochs=1000, learning_rate=1.2)
 
-# test
-result = net.predict(X, Y)
-print(result)
+# create our testing dataset
+X_test = np.random.randint(2, size = (100, 2))
+
+mask1 = X_test[:, 0] > 0.5
+mask2 = X_test[:, 1] > 0.5
+
+Y_test = np.logical_xor(mask1, mask2)
+Y_test = Y_test.reshape(100, 1)
+
+X_test = X_test.T
+Y_test = Y_test.T
+
+# testing!
+result = net.predict(X_test)
+result = np.where(result > 0.5, 1., 0.)[0]
+print("Accuracy:", np.mean(result == Y_test), "out of 1")